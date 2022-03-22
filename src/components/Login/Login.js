f88b3399cfa5605b52c56248066eaf0599ca5879
Hiresh <Hiresh3p@gmail.com>
2022-03-22 13:24:25 +0400

    + removed unneccessary validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -32,10 +32,7 @@ const validationSchema = yup.object({
     .string("Enter your email")
     .email("Enter a valid email")
     .required("Email is required"),
-  password: yup
-    .string("Enter your password")
-    .min(8, "Password should be of minimum 8 characters length")
-    .required("Password is required"),
+  password: yup.string("Enter your password").required("Password is required"),
 });
 
 const Login = () => {
