a8c83b0897bd400142e583e14b1a5cd8869135e5
Hiresh <Hiresh3p@gmail.com>
2022-04-01 19:18:20 +0400

    +Drawer changes

--- src/components/Dashboard/Drawer/Drawer.js
+++ src/components/Dashboard/Drawer/Drawer.js
@@ -39,10 +39,8 @@ const Sidebar = () => {
         const docRef = doc(db, "tutors", user.uid);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
-          // console.log("Document data:", docSnap.data());
           setUserDetails(docSnap.data());
         } else {
-          // doc.data() will be undefined in this case
           console.log("No such document!");
         }
       }
