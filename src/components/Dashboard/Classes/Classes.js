fd24be5dd09fc0f5e488a2371d799a0918ac8eb9
Hiresh <Hiresh3p@gmail.com>
2022-04-10 22:00:06 +0400

    +Changes on classes

--- src/components/Dashboard/Classes/Classes.js
+++ src/components/Dashboard/Classes/Classes.js
@@ -145,7 +145,7 @@ const Classes = () => {
                       >
                         <AvatarContainer
                           alt="Remy Sharp"
-                          src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                          src={userDetails.profilePic}
                         />
                       </CardContent>
                     )}
