24d9bdcb11c9bf772c80bb9a95160bd01c4ee607
Hiresh <Hiresh3p@gmail.com>
2022-04-22 14:50:36 +0400

    +DeleteStudentEmail

--- src/components/Dashboard/ViewProfile/DeleteStudentProfile.js
+++ src/components/Dashboard/ViewProfile/DeleteStudentProfile.js
@@ -28,7 +28,7 @@ import {
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-const DeleteTutorProfile = () => {
+const DeleteStudentProfile = () => {
   const { userDetails, user } = useUserAuth();
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -71,35 +71,23 @@ const DeleteTutorProfile = () => {
       const credential = EmailAuthProvider.credential(user.email, password);
       await reauthenticateWithCredential(user, credential)
         .then(async () => {
-          await deleteSubmittedAssignments()
-            .then(async () => {
-              await deleteAssignments();
-            })
-            .then(async () => {
-              await deleteAnnouncements();
-            })
-            .then(async () => {
-              await deleteBookings();
-            })
-            .then(async () => {
-              await deleteJoinedClasses();
-            })
-            .then(async () => {
-              await deleteCreatedClasses();
-            })
-            .then(async () => {
-              await deleteDoc(doc(db, "tutors", user?.uid))
-                .then(async () => {
-                  await deleteUser(user).then(() => {
-                    navigate("/");
+          await deleteSubmittedAssignments().then(async () => {
+            await deleteBookings().then(async () => {
+              await deleteJoinedClasses().then(async () => {
+                await deleteDoc(doc(db, "students", user?.uid))
+                  .then(async () => {
+                    await deleteUser(user).then(() => {
+                      navigate("/");
+                    });
+                  })
+                  .catch((err) => {
+                    setSnackBarOpen(true);
+                    setMessage("An error occurred, please try again");
+                    setLoading(false);
                   });
-                })
-                .catch((err) => {
-                  setSnackBarOpen(true);
-                  setMessage("An error occurred, please try again");
-                  setLoading(false);
-                });
+              });
             });
+          });
         })
         .catch((err) => {
           if (err.message.includes("auth/wrong-password")) {
@@ -118,87 +106,17 @@ const DeleteTutorProfile = () => {
   // delete submittedAssignments
   const deleteSubmittedAssignments = async () => {
     const dataArray = [];
-    const newArray = [];
     const q = query(
-      collection(db, "createdClasses"),
-      where("tutorEmail", "==", userDetails?.email)
+      collection(db, "submittedAssignments"),
+      where("studentEmail", "==", userDetails?.email)
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       dataArray.push(doc.id);
     });
 
-    for await (const data of dataArray) {
-      const q = query(
-        collection(db, "submittedAssignments"),
-        where("classCode", "==", data)
-      );
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(async (doc) => {
-        newArray.push(doc.id);
-      });
-    }
-
-    newArray.map((values) => {
+    dataArray.map((values) => {
       return deleteDoc(doc(db, "submittedAssignments", values));
-    });
-  };
-
-  // delete Assignments
-  const deleteAssignments = async () => {
-    const dataArray = [];
-    const newArray = [];
-    const q = query(
-      collection(db, "createdClasses"),
-      where("tutorEmail", "==", userDetails?.email)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      dataArray.push(doc.id);
-    });
-
-    for await (const data of dataArray) {
-      const q = query(
-        collection(db, "assignments"),
-        where("classCode", "==", data)
-      );
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(async (doc) => {
-        newArray.push(doc.id);
-      });
-    }
-
-    newArray.map((values) => {
-      return deleteDoc(doc(db, "assignments", values));
-    });
-  };
-
-  // delete Announcements
-  const deleteAnnouncements = async () => {
-    const dataArray = [];
-    const newArray = [];
-    const q = query(
-      collection(db, "createdClasses"),
-      where("tutorEmail", "==", userDetails?.email)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      dataArray.push(doc.id);
-    });
-
-    for await (const data of dataArray) {
-      const q = query(
-        collection(db, "announcements"),
-        where("classCode", "==", data)
-      );
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(async (doc) => {
-        newArray.push(doc.id);
-      });
-    }
-
-    newArray.map((values) => {
-      return deleteDoc(doc(db, "announcements", values));
     });
   };
 
@@ -207,7 +125,7 @@ const DeleteTutorProfile = () => {
     const dataArray = [];
     const q = query(
       collection(db, "bookings"),
-      where("tutorEmail", "==", userDetails?.email)
+      where("studentEmail", "==", userDetails?.email)
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
@@ -224,7 +142,7 @@ const DeleteTutorProfile = () => {
     const dataArray = [];
     const q = query(
       collection(db, "joinedClasses"),
-      where("tutorEmail", "==", userDetails?.email)
+      where("studentEmail", "==", userDetails?.email)
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
@@ -233,23 +151,6 @@ const DeleteTutorProfile = () => {
 
     dataArray.map((values) => {
       return deleteDoc(doc(db, "joinedClasses", values));
-    });
-  };
-
-  // delete createdClasses
-  const deleteCreatedClasses = async () => {
-    const dataArray = [];
-    const q = query(
-      collection(db, "createdClasses"),
-      where("tutorEmail", "==", userDetails?.email)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      dataArray.push(doc.id);
-    });
-
-    dataArray.map((values) => {
-      return deleteDoc(doc(db, "createdClasses", values));
     });
   };
 
@@ -344,4 +245,4 @@ const DeleteTutorProfile = () => {
   );
 };
 
-export default DeleteTutorProfile;
+export default DeleteStudentProfile;
