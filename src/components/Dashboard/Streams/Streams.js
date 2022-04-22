0ba914a06ec9dc98823552dab8150f0381153b3d
Hiresh <Hiresh3p@gmail.com>
2022-04-22 23:18:06 +0400

    +Streams

--- src/components/Dashboard/Streams/Streams.js
+++ src/components/Dashboard/Streams/Streams.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import {
   Box,
@@ -7,9 +7,18 @@ import {
   Button,
   LinearProgress,
   Snackbar,
+  Grid,
+  Typography,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import { storage } from "../../../firebase-config";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+  deleteObject,
+} from "firebase/storage";
 import { db } from "../../../firebase-config";
 import {
   doc,
@@ -19,9 +28,15 @@ import {
   arrayUnion,
   addDoc,
   serverTimestamp,
+  orderBy,
+  onSnapshot,
+  query,
+  where,
+  deleteDoc,
+  arrayRemove,
 } from "firebase/firestore";
 import { useUserAuth } from "../../../Context/UserAuthContext";
-import ViewStreams from "./ViewStreams";
+import ShowStreamsIcon from "./ShowStreamsIcon";
 
 const Streams = () => {
   const location = useLocation();
@@ -36,6 +51,7 @@ const Streams = () => {
   const [snackBarOpen, setSnackBarOpen] = useState(false);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showFiles, setShowFiles] = useState([]);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -115,6 +131,7 @@ const Streams = () => {
           grade: classGrade,
           classCode: classCode,
           title: announcementValue,
+          timestamp: serverTimestamp(),
         });
         setSnackBarOpen(true);
         setMessage("Stream created successfully");
@@ -131,6 +148,73 @@ const Streams = () => {
       return;
     }
   };
+
+  // Delete a stream
+  const handleDeleteStream = async (streamId, streamName, fileName) => {
+    let confirmAction = window.confirm("Are you sure to delete " + streamName);
+    if (confirmAction) {
+      try {
+        fileName.map(async (file) => {
+          const filePathRef = ref(
+            storage,
+            "streams/" + classCode + "/" + streamId + "/" + file
+          );
+          await deleteObject(filePathRef);
+        });
+
+        await deleteDoc(doc(db, "announcements", streamId));
+        setSnackBarOpen(true);
+        setMessage("Deleted Successfully");
+      } catch (err) {
+        setSnackBarOpen(true);
+        setMessage("An error occurred, please try again");
+      }
+    }
+  };
+
+  // Delete one material in a stream
+  const handleObjectDelete = async (streamId, title, fileName, fileUrl) => {
+    let confirmAction = window.confirm(
+      "Are you sure to delete " + fileName + " from " + title
+    );
+
+    if (confirmAction) {
+      try {
+        const filePathRef = ref(
+          storage,
+          "streams/" + classCode + "/" + streamId + "/" + fileName
+        );
+        await deleteObject(filePathRef).then(async () => {
+          const deleteFirestoreRef = doc(db, "announcements", streamId);
+          await updateDoc(deleteFirestoreRef, {
+            fileName: arrayRemove(fileName),
+            fileUrl: arrayRemove(fileUrl),
+          });
+        });
+
+        setSnackBarOpen(true);
+        setMessage("Deleted Successfully");
+      } catch (err) {
+        setSnackBarOpen(true);
+        setMessage("An error occurred, please try again");
+      }
+    }
+  };
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "announcements"),
+      where("classCode", "==", classCode),
+      orderBy("timestamp")
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const newFiles = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setShowFiles(newFiles);
+    });
+  }, []);
 
   return (
     <>
@@ -179,6 +263,87 @@ const Streams = () => {
           />
         </Box>
       )}
+
+      {showFiles?.map((showFile) => {
+        return (
+          <Box sx={{ boxShadow: 5, mt: 5 }} key={showFile.id + 1}>
+            <Paper>
+              <Box>
+                <Paper>
+                  <Box
+                    fullWidth
+                    sx={{
+                      p: 1,
+                      backgroundColor: "#c5c6c7",
+                      display: "flex",
+                    }}
+                  >
+                    <h5 style={{ flex: 1 }}>{showFile.title}</h5>
+                    {userDetails?.accountType === "Tutor" && (
+                      <CloseIcon
+                        sx={{ color: "#870000", cursor: "pointer" }}
+                        onClick={() => {
+                          handleDeleteStream(
+                            showFile.id,
+                            showFile.title,
+                            showFile.fileName
+                          );
+                        }}
+                      />
+                    )}
+                  </Box>
+                </Paper>
+              </Box>
+              <Grid container spacing={2} sx={{ p: 2 }}>
+                {showFile.fileName?.map((showFilesName, index) => {
+                  return (
+                    <Grid item md={3} xs={12} key={index}>
+                      <Box
+                        sx={{
+                          boxShadow: 1,
+                          textAlign: "center",
+                        }}
+                      >
+                        <Paper sx={{ p: 1 }}>
+                          {userDetails?.accountType === "Tutor" && (
+                            <RemoveCircleOutlineIcon
+                              sx={{
+                                color: "red",
+                                fontSize: 20,
+                                float: "right",
+                                cursor: "pointer",
+                              }}
+                              onClick={() =>
+                                handleObjectDelete(
+                                  showFile.id,
+                                  showFile.title,
+                                  showFilesName,
+                                  showFile.fileUrl[index]
+                                )
+                              }
+                            />
+                          )}
+                          <a
+                            href={showFile.fileUrl[index]}
+                            target="_blank"
+                            rel="noreferrer"
+                            style={{ textDecoration: "none", color: "#000" }}
+                          >
+                            <Box p={1}>
+                              <ShowStreamsIcon fileName={showFilesName} />
+                              <Typography>{showFilesName}</Typography>
+                            </Box>
+                          </a>
+                        </Paper>
+                      </Box>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Paper>
+          </Box>
+        );
+      })}
 
       <Snackbar
         open={snackBarOpen}
