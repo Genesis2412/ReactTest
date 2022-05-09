6e350a14699caf846079637ea8a5d27642a839d3
Hiresh <Hiresh3p@gmail.com>
2022-05-09 10:42:50 +0400

    +Videocall system

--- src/components/Dashboard/Videocall/Videocall.js
+++ src/components/Dashboard/Videocall/Videocall.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { JitsiMeeting } from "@jitsi/react-sdk";
+import { useUserAuth } from "../../../Context/UserAuthContext";
 
 const Videocall = () => {
+  const { userDetails } = useUserAuth();
+  const name = userDetails?.name?.firstName + " " + userDetails?.name?.lastName;
+
   return (
-    <>
-      <div>Videocall</div>
-    </>
+    <JitsiMeeting
+      roomName={"Newmeetsxcw"}
+      getIFrameRef={(iframeRef) => {
+        iframeRef.style.height = "800px";
+      }}
+      userInfo={{
+        displayName: name,
+      }}
+      configOverwrite={{
+        startWithAudioMuted: true,
+        startWithVideoMuted: true,
+      }}
+    />
   );
 };
 
