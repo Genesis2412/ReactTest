5bec96ce3f876a930ddc6e8d3e5e6901ef0a659e
Hiresh <Hiresh3p@gmail.com>
2022-05-10 14:00:19 +0400

    +Backend starter

---
+++ server/controllers/auth.js
@@ -0,0 +1,31 @@
+const { connect } = require("getstream");
+const bcrypt = require("bcrypt");
+const StreamChat = require("stream-chat");
+const crypto = require("crypto");
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const userId = crypto.randomBytes(16).toString("hex");
+    const serverClient = connect(api_key, api_secret, app_id);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createUserToken(userId);
+
+    res.status(200).json({ token, firstName, lastName, email, hashedPassword });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+  } catch (error) {}
+};
+
+module.exports = { register, login };
