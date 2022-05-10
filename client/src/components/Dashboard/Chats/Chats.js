4d01b2683fcf122b2b2dafae91d6184e3928201e
Hiresh <Hiresh3p@gmail.com>
2022-05-10 20:32:43 +0400

    +ChatSystem halfthrough

--- client/src/components/Dashboard/Chats/Chats.js
+++ client/src/components/Dashboard/Chats/Chats.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import { ChannelListContainer, ChannelContainer } from "./exportsFiles";
+import "stream-chat-react/dist/css/index.css";
 import "./Chats.css";
 
 const apiKey = "k248hxcdpdqk";
 const client = StreamChat.getInstance(apiKey);
+const cookies = new Cookies();
 
 const Chats = () => {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      client.connectUser(
+        {
+          id: cookies.get("token"),
+          id: cookies.get("userId"),
+          // firstName: data?.firstName,
+          // lastName: data?.lastName,
+          // email: data?.email,
+        },
+        cookies.get("token")
+      );
+    };
+
+    return () => {
+      getUsers();
+    };
+  }, []);
+
   return (
     <>
       <div className="app__wrapper">
         <Chat client={client} theme="team light">
-          <ChannelListContainer />
-          <ChannelContainer />
+          <ChannelListContainer
+            isCreating={isCreating}
+            setIsCreating={setIsCreating}
+            setCreateType={setCreateType}
+            setIsEditing={setIsEditing}
+          />
+          <ChannelContainer
+            isCreating={isCreating}
+            setIsCreating={setIsCreating}
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+            createType={createType}
+          />
         </Chat>
       </div>
     </>
---
+++ client/src/components/Dashboard/Chats/TeamMessage.js
@@ -0,0 +1,67 @@
+import React, { useRef } from "react";
+import {
+  Attachment,
+  Avatar,
+  messageHasReactions,
+  MessageOptions,
+  MessageRepliesCountButton,
+  MessageStatus,
+  MessageText,
+  MessageTimestamp,
+  ReactionSelector,
+  SimpleReactionsList,
+  useMessageContext,
+  MessageSimple,
+} from "stream-chat-react";
+
+import "./TeamMessage.scss";
+
+const TeamMessage = () => {
+  const {
+    isReactionEnabled,
+    message,
+    reactionSelectorRef,
+    showDetailedReactions,
+  } = useMessageContext();
+
+  const messageWrapperRef = useRef(null);
+
+  const hasReactions = messageHasReactions(message);
+
+  return (
+    <div className="message-wrapper">
+      <Avatar
+        image={message.user?.image}
+        name={message.user?.firstName + " " + message.user?.lastName}
+      />
+      <div className="message-wrapper-content">
+        <MessageOptions
+          displayLeft={false}
+          messageWrapperRef={messageWrapperRef}
+        />
+        <div className="message-header">
+          <div className="message-header-name">
+            {message.user?.firstName + " " + message.user?.lastName}
+          </div>
+          <div className="message-header-timestamp">
+            <MessageTimestamp />
+          </div>
+        </div>
+        {showDetailedReactions && isReactionEnabled && (
+          <ReactionSelector ref={reactionSelectorRef} />
+        )}
+        <MessageText />
+        <MessageStatus />
+        {message.attachments && (
+          <Attachment attachments={message.attachments} />
+        )}
+        {hasReactions && !showDetailedReactions && isReactionEnabled && (
+          <SimpleReactionsList />
+        )}
+        <MessageRepliesCountButton reply_count={message.reply_count} />
+      </div>
+    </div>
+  );
+};
+
+export default TeamMessage;
