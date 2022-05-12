14e12301e818358ee1bd95d4aa6b7fb20f8cb66f
Hiresh <Hiresh3p@gmail.com>
2022-05-12 09:58:41 +0400

    +CreateChannel styling

--- client/src/components/Dashboard/Chats/CreateChannel.js
+++ client/src/components/Dashboard/Chats/CreateChannel.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useChatContext } from "stream-chat-react";
 import { UserList } from "./exportsFiles";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { Button, Typography, Box, TextField } from "@mui/material";
 
 const ChannelNameInput = ({ channelName = "", setChannelName }) => {
   const handleChange = (event) => {
@@ -10,15 +11,14 @@ const ChannelNameInput = ({ channelName = "", setChannelName }) => {
   };
 
   return (
-    <div className="channel-name-input__wrapper">
-      <p>Name</p>
-      <input
+    <Box sx={{ p: 3, boxShadow: "0px 1px 0px rgba(0, 0, 0, 0.1)" }}>
+      <TextField
         value={channelName}
         onChange={handleChange}
-        placeholder="channel-name"
+        label="Enter Channel Name"
+        fullWidth
       />
-      <p>Add members</p>
-    </div>
+    </Box>
   );
 };
 
@@ -47,15 +47,25 @@ const CreateChannel = ({ createType, setIsCreating }) => {
   };
 
   return (
-    <div className="create-channel__container">
-      <div className="create-channel__header">
-        <p>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: "62px",
+          paddingRight: "20px",
+          boxShadow: 2,
+        }}
+      >
+        <Typography sx={{ pl: 2, color: "#0b0c10" }}>
           {createType === "team"
             ? "Create a New Channel"
             : "Send a Direct Message"}
-        </p>
-        <CancelIcon setIsCreating={setIsCreating} />
-      </div>
+        </Typography>
+        <CancelIcon setIsCreating={setIsCreating} sx={{ float: "right" }} />
+      </Box>
+
       {createType === "team" && (
         <ChannelNameInput
           channelName={channelName}
@@ -63,12 +73,22 @@ const CreateChannel = ({ createType, setIsCreating }) => {
         />
       )}
       <UserList setSelectedUsers={setSelectedUsers} />
-      <div className="create-channel__button-wrapper" onClick={createChannel}>
-        <p>
-          {createType === "team" ? "Create Channel" : "Create Message Group"}
-        </p>
-      </div>
-    </div>
+      <Button
+        onClick={createChannel}
+        sx={[
+          {
+            "&:hover": {
+              backgroundColor: "#c5c6c7",
+              color: "#0b0c10",
+            },
+            backgroundColor: "#45a29e",
+            color: "#fff",
+          },
+        ]}
+      >
+        {createType === "team" ? "Create Channel" : "Create Message"}
+      </Button>
+    </Box>
   );
 };
 
