7ad3ca71700bff0906a9370ba23557be0c4082f4
Hiresh <Hiresh3p@gmail.com>
2022-05-15 19:21:56 +0400

    +ViewMembers error handling

--- client/src/components/Dashboard/Chats/ViewMembers.js
+++ client/src/components/Dashboard/Chats/ViewMembers.js
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 import { useChatContext } from "stream-chat-react";
 import { MemberList } from "./exportsFiles";
 import CloseIcon from "@mui/icons-material/Close";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Snackbar } from "@mui/material";
 
 const ViewMembers = ({ setIsViewing }) => {
   const [selectedUsers, setSelectedUsers] = useState([]);
   const { channel } = useChatContext();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
 
   const updateChannel = async (e) => {
     try {
@@ -20,46 +29,57 @@ const ViewMembers = ({ setIsViewing }) => {
         });
       }
     } catch (error) {
-      console.log(error);
+      setSnackbarOpen(true);
+      setSnackbarMessage("An error occurred, please try again");
     }
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          height: "62px",
-          paddingRight: "20px",
-          boxShadow: 2,
-        }}
-      >
-        <Typography sx={{ pl: 2, color: "#0b0c10" }}>View Members</Typography>
-        <CloseIcon
-          onClick={() => setIsViewing(false)}
-          sx={{ cursor: "pointer" }}
-        />
+    <>
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "62px",
+            paddingRight: "20px",
+            boxShadow: 2,
+          }}
+        >
+          <Typography sx={{ pl: 2, color: "#0b0c10" }}>View Members</Typography>
+          <CloseIcon
+            onClick={() => setIsViewing(false)}
+            sx={{ cursor: "pointer" }}
+          />
+        </Box>
+
+        <MemberList setSelectedUsers={setSelectedUsers} />
+        <Button
+          onClick={updateChannel}
+          sx={[
+            {
+              "&:hover": {
+                backgroundColor: "#c5c6c7",
+                color: "#0b0c10",
+              },
+              backgroundColor: "#45a29e",
+              color: "#fff",
+            },
+          ]}
+        >
+          Delete members
+        </Button>
       </Box>
 
-      <MemberList setSelectedUsers={setSelectedUsers} />
-      <Button
-        onClick={updateChannel}
-        sx={[
-          {
-            "&:hover": {
-              backgroundColor: "#c5c6c7",
-              color: "#0b0c10",
-            },
-            backgroundColor: "#45a29e",
-            color: "#fff",
-          },
-        ]}
-      >
-        Delete members
-      </Button>
-    </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message={snackbarMessage}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      />
+    </>
   );
 };
 
