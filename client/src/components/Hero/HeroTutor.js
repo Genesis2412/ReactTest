ee113186007533a1630b9c487a7723e1569b2a00
Hiresh <Hiresh3p@gmail.com>
2022-06-14 18:53:00 +0400

    +HeroTutor animations

--- client/src/components/Hero/HeroTutor.js
+++ client/src/components/Hero/HeroTutor.js
@@ -22,32 +22,77 @@ import TutorAssignmentDiagram from "../../images/Hero/TutorAssignmentDiagram.jpg
 import StudentChatDiagram from "../../images/Hero/StudentChatDiagram.jpg";
 import StudentJoinBanner from "../../images/Hero/StudentJoinBanner.jpg";
 import TutorVideocallDiagram from "../../images/Hero/TutorVideocallDiagram.jpg";
+import ParallaxOne from "../../images/Hero/ParallaxOne.jpg";
+import ParallaxTwo from "../../images/Hero/ParallaxTwo.jpg";
+import ParallaxThree from "../../images/Hero/ParallaxThree.jpg";
 import { motion } from "framer-motion";
 
 const HeroTutor = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.7 }}
-    >
+    <>
       <Box sx={{ mt: 25, textAlign: "center" }}>
-        <Logo to="/">MauTutorz</Logo>
-        <Typography sx={{ mt: 4, fontSize: 25 }}>
+        <Logo
+          to="/"
+          as={motion.span}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.3, duration: 0.7, ease: "easeInOut" },
+          }}
+        >
+          MauTutorz
+        </Logo>
+        <Typography
+          sx={{ mt: 4, fontSize: 25 }}
+          as={motion.p}
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
+          }}
+        >
           Your new online tutoring platform
         </Typography>
-        <Typography sx={{ fontSize: 16 }}>
+        <Typography
+          sx={{ fontSize: 16 }}
+          as={motion.p}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.7, duration: 0.7, ease: "easeInOut" },
+          }}
+        >
           MauTutorz is your go-to e-tutoring platform. Our intutive user
           interface allows you to focus only on learning rather than learning
           the system.
         </Typography>
-        <Typography sx={{ fontSize: 16 }}>
+        <Typography
+          sx={{ fontSize: 16 }}
+          as={motion.p}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.8, duration: 0.7, ease: "easeInOut" },
+          }}
+        >
           Everything is integrated in the system. No need for unnecessary
           navigations.
         </Typography>
 
-        <Typography sx={{ fontSize: 18, mt: 2 }}>
+        <Typography
+          sx={{ fontSize: 18, mt: 2 }}
+          as={motion.p}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { delay: 0.9, duration: 0.7, ease: "easeInOut" },
+          }}
+        >
           You pay-as-you-go. You will only pay for how much resources you use.
         </Typography>
         <Link to="/register" style={{ textDecoration: "none" }}>
@@ -77,8 +122,29 @@ const HeroTutor = () => {
           justifyContent="center"
           alignItems="center"
         >
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ x: -1000 }}
+            animate={{
+              x: 0,
+              transition: { delay: 1, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <EventIcon sx={{ fontSize: 35, color: "#2DA2E1" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Control your bookings
@@ -87,8 +153,29 @@ const HeroTutor = () => {
             </Paper>
           </Grid>
 
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ y: -1000 }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.1, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <SchoolIcon sx={{ fontSize: 35, color: "#F47D2E" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Learning Management System (LMS)
@@ -97,8 +184,29 @@ const HeroTutor = () => {
             </Paper>
           </Grid>
 
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ y: -1000 }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.2, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <AssignmentIcon sx={{ fontSize: 35, color: "#A2B458" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Manage Assignments
@@ -107,8 +215,29 @@ const HeroTutor = () => {
             </Paper>
           </Grid>
 
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ y: -1000 }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.3, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <ChatIcon sx={{ fontSize: 35, color: "#886CC4" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Integrated chat system
@@ -119,8 +248,29 @@ const HeroTutor = () => {
             </Paper>
           </Grid>
 
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ y: -1000 }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.4, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <VideoCallIcon sx={{ fontSize: 35, color: "#D44C2C" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Integrated Videoconferencing system
@@ -129,8 +279,29 @@ const HeroTutor = () => {
             </Paper>
           </Grid>
 
-          <Grid item md={12 / 6} xs={12}>
-            <Paper sx={{ p: 2, boxShadow: 3 }}>
+          <Grid
+            item
+            md={12 / 6}
+            xs={12}
+            as={motion.div}
+            initial={{ y: -1000 }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.5, duration: 1, type: "spring" },
+            }}
+          >
+            <Paper
+              sx={{ p: 2, boxShadow: 3 }}
+              as={motion.div}
+              animate={{
+                y: [5, 0, 5],
+                transition: {
+                  duration: 2,
+                  ease: "linear",
+                  repeat: "Infinity",
+                },
+              }}
+            >
               <LockIcon sx={{ fontSize: 35, color: "#F1A81A" }} />
               <Typography sx={{ fontWeight: "bold" }}>
                 Secured by design
@@ -144,18 +315,29 @@ const HeroTutor = () => {
       </Box>
 
       {/* Bookings */}
-      <Box sx={{ textAlign: "center", p: 2, boxShadow: 5 }}>
-        <Typography sx={{ fontSize: 25 }}>Bookings</Typography>
-        <TutorBookingImg src={TutorBookingDiagram} alt={"picture"} />
-        <Typography sx={{ mt: 1 }}>
-          Control the access to your classes.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          Choose to accept or decline student bookings.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          Students cannot join any of your classes without your authorisation.
-        </Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          boxShadow: 5,
+          background: `url(${ParallaxOne})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          color: "#fff",
+        }}
+      >
+        <Box sx={{ backgroundColor: "rgba(0, 0, 0, 0.46)", p: 2 }}>
+          <Typography sx={{ fontSize: 25 }}>Bookings</Typography>
+          <TutorBookingImg src={TutorBookingDiagram} alt={"picture"} />
+          <Typography sx={{ mt: 1 }}>
+            Control the access to your classes.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Choose to accept or decline student bookings.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Students cannot join any of your classes without your authorisation.
+          </Typography>
+        </Box>
       </Box>
 
       {/* LMS */}
@@ -177,22 +359,36 @@ const HeroTutor = () => {
       </Box>
 
       {/* Assignments */}
-      <Box sx={{ textAlign: "center", mt: 5, p: 2, boxShadow: 5 }}>
-        <Typography sx={{ fontSize: 25 }}>
-          Manage Assignments - Grading, remarks and share corrected files
-        </Typography>
-        <TutorAssignmentImg src={TutorAssignmentDiagram} alt={"picture"} />
-        <Typography sx={{ mt: 1 }}>
-          Create and share your assignment with your students.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          View all your students submissions at ease.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>Grade and add remarks easily.</Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          Need to share your student corrected file. Just send it on the
-          submission.
-        </Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          mt: 5,
+          boxShadow: 5,
+          background: `url(${ParallaxTwo})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          color: "#fff",
+        }}
+      >
+        <Box sx={{ backgroundColor: "rgba(0, 0, 0, 0.46)", p: 2 }}>
+          <Typography sx={{ fontSize: 25 }}>
+            Manage Assignments - Grading, remarks and share corrected files
+          </Typography>
+          <TutorAssignmentImg src={TutorAssignmentDiagram} alt={"picture"} />
+          <Typography sx={{ mt: 1 }}>
+            Create and share your assignment with your students.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            View all your students submissions at ease.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Grade and add remarks easily.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Need to share your student corrected file. Just send it on the
+            submission.
+          </Typography>
+        </Box>
       </Box>
 
       {/* Integrated chat system */}
@@ -212,20 +408,32 @@ const HeroTutor = () => {
       </Box>
 
       {/* Videocall */}
-      <Box sx={{ textAlign: "center", p: 2, boxShadow: 5, mt: 5 }}>
-        <Typography sx={{ fontSize: 25 }}> Videoconferencing</Typography>
-        <TutorVideocallImg src={TutorVideocallDiagram} alt={"picture"} />
-        <Typography sx={{ mt: 1 }}>
-          Carry out your classes with our powerful videoconferencing system
-          allowing about concurrent 100 users.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          Chat with participants and create polls.
-        </Typography>
-        <Typography sx={{ mt: 0.5 }}>
-          Get the most from the conferencing system like share screen, record
-          session etc..
-        </Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          boxShadow: 5,
+          mt: 5,
+          background: `url(${ParallaxThree})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          color: "#fff",
+        }}
+      >
+        <Box sx={{ backgroundColor: "rgba(0, 0, 0, 0.46)", p: 2 }}>
+          <Typography sx={{ fontSize: 25 }}> Videoconferencing</Typography>
+          <TutorVideocallImg src={TutorVideocallDiagram} alt={"picture"} />
+          <Typography sx={{ mt: 1 }}>
+            Carry out your classes with our powerful videoconferencing system
+            allowing about concurrent 100 users.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Chat with participants and create polls.
+          </Typography>
+          <Typography sx={{ mt: 0.5 }}>
+            Get the most from the conferencing system like share screen, record
+            session etc..
+          </Typography>
+        </Box>
       </Box>
 
       {/* Get Started */}
@@ -265,7 +473,7 @@ const HeroTutor = () => {
           All rights reserved
         </Typography>
       </Box>
-    </motion.div>
+    </>
   );
 };
 
