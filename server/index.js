59200fa54d027c677f900d5baf139457634f26f5
Hiresh <Hiresh3p@gmail.com>
2022-07-04 18:55:16 +0400

    +HTTP header changes

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
